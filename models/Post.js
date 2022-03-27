69375f9b5b3d7877927c967246b031d16a0d6b73
Arjun Singh <arjunkhakh@gmail.com>
2022-03-27 23:49:03 +0100

    adding a database and adding my models and starting to seed the js pages

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init({
+
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+     },
+
+    comment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            max: 200
+        }
+    },
+
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+})
+
+module.exports = Post;
